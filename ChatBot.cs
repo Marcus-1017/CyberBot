a5e41923b05795476a7aee6714596703c3d746a7
marcus johnson <ST10496028@rcconnect.edu.za>
2026-04-12 05:16:55 +0200

    simple name handling + ascii art and audio greeting handling

---
+++ ChatBot.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+//Marcus Johnson
+//ST10496028
+
+public class ChatBot
+{
+    public string Name; 
+
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,23 @@
-﻿Console.WriteLine("hello world!");
+﻿using System;
+
+//Marcus Johnson
+//ST10496028
+
+ConsoleUI.PlayGreetingAudio();
+
+ConsoleUI.DisplayAsciiArt();
+Console.WriteLine("hi there! what is your name??");
+
+string Name = Console.ReadLine();
+
+while (string.IsNullOrWhiteSpace(Name))
+{
+    Console.WriteLine("Names can't be blank! Please try again.");
+    Name = Console.ReadLine();
+
+}
+
+ChatBot bot = new();
+
+bot.Name = Name;
+
